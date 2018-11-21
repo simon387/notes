38ed2b1ae23847a803031eaf31ca6dce0ce5a1cf
Simone Celia <simon387@hotmail.it>
2018-11-21 14:07:51 +0100

    miotitolodicommit

--- lazygit.bat
+++ lazygit.bat
@@ -1,6 +1,6 @@
 echo ciccione!
 ::#region
 git add .
-if [%1]==[] (git commit -m "lazy") else (git commit -m %1)
+if [%1]==[] (git commit -m "lazy") else (git commit -m "%1")
 git push
 ::#endregion
