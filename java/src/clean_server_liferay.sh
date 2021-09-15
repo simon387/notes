a6796b30692248a76bd6cfcbdb1660573424c449
simone celia <simon387@hotmail.it>
2021-09-15 09:21:00 +0200

    Add files via upload

---
+++ java/src/clean_server_liferay.sh
@@ -0,0 +1,5 @@
+rm -rf /home/scelia/dev/top-barcelona/quid/server/liferay-portal-7.1.2-ga3/osgi/modules/*
+rm -rf /home/scelia/dev/top-barcelona/quid/server/liferay-portal-7.1.2-ga3/osgi/state/*
+rm -rf /home/scelia/dev/top-barcelona/quid/server/liferay-portal-7.1.2-ga3/work/*
+rm -rf /home/scelia/dev/top-barcelona/quid/server/liferay-portal-7.1.2-ga3/tomcat-9.0.10/temp/*
+rm -rf /home/scelia/dev/top-barcelona/quid/server/liferay-portal-7.1.2-ga3/tomcat-9.0.10/work/*
